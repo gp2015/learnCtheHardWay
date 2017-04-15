719218026b088da96d9bc17da0b4943747334810
gp2015 <georgec.perez@gmail.com>
2017-04-15 14:04:29 -0400

    Completed banas vid 1 and tutorial3_1.c

--- 0-misc/0-banas/tutorial3_1.c
+++ 0-misc/0-banas/tutorial3_1.c
@@ -3,7 +3,7 @@
 // Needed for exit()
 #include <stdlib.h>
 
-void main() {
+int main() {
 
 	int whatToDo = 0;
 
@@ -11,11 +11,56 @@ void main() {
 		printf("\n");
 		printf("1. What Time is it?\n");
 		printf("2. What is the date?\n");
-		printf("3. What day is it?\n";
+		printf("3. What day is it?\n");
 		printf("4. Quit\n");
 
 		scanf(" %d", &whatToDo);
-		
+
 	} while (whatToDo < 1 || whatToDo > 4);
-	exit(0)
+
+
+
+	if (whatToDo == 1) {
+		printf("Print the time\n");
+	}
+	else if (whatToDo == 2) {
+		printf("Print the date\n");
+	}
+	else if (whatToDo == 3) {
+		printf("Print the day\n");
+	}
+	else {
+		printf("Bye bye\n");
+
+		// Almost always better to use return
+		// exit(0); // commented out to exit in below switch
+	}
+
+
+
+	printf("\n");
+
+
+
+	switch(whatToDo) {
+		case(1):
+			printf("Print the time\n");
+			break;
+
+		case(2):
+			printf("Print the date\n");
+			break;
+
+		case(3):
+			printf("Print the day\n");
+			break;
+
+		default:
+			printf("Bye bye\n");
+			exit(0);
+			break;
+	}
+
+	return 0;
+
 }
